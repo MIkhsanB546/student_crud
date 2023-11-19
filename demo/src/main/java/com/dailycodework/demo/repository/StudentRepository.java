24c49c50de3d9e1b6635345d07405a67be863b10
MIkhsanB546 <mikhsan546@gmail.com>
2023-11-19 14:27:10 +0700

    first commit

---
+++ demo/src/main/java/com/dailycodework/demo/repository/StudentRepository.java
@@ -0,0 +1,13 @@
+package com.dailycodework.demo.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dailycodework.demo.model.Student;
+
+public interface StudentRepository extends JpaRepository<Student, Long> {
+
+  Optional<Student> findByEmail(String email);
+  
+}
---
+++ demo/src/main/java/com/dailycodework/demo/service/IStudentService.java
@@ -0,0 +1,17 @@
+package com.dailycodework.demo.service;
+
+import java.util.List;
+
+import com.dailycodework.demo.model.Student;
+
+public interface IStudentService {
+  Student addStudent(Student student);
+
+  List<Student> getStudents();
+
+  Student updateStudent(Student student, Long id);
+
+  Student getStudentById(Long id);
+
+  void deleteStudent(Long id);
+}
